84f75ff27bcdc4d94c1ed85219c51a383defa877
Sahil <garg07825@gmail.com>
2023-08-04 19:09:23 +0530

    Start the server

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from "express";
+const port = 3000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("hii");
+});
+
+// server listen
+app.listen(port, (e) => {
+  if (e) return console.log(e);
+  return console.log("serber is working on 3000");
+});
